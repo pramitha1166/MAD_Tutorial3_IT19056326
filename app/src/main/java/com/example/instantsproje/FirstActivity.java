c0f16ebf10369a744634ca39ef801da7c1d02873
pramitha1166 <pramitha.wanigarathne@gmail.com>
2020-08-19 00:15:57 +0530

    Add Custom Toast Massage

--- app/src/main/java/com/example/instantsproje/FirstActivity.java
+++ app/src/main/java/com/example/instantsproje/FirstActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +22,19 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LayoutInflater li = getLayoutInflater();
+
+        View layout = li.inflate(R.layout.customtost, (ViewGroup)findViewById(R.id.custom_toast_layout));
+
+        Toast toast = new Toast(getApplicationContext());
+
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+
+        toast.setView(layout);
+
+        toast.show();
 
 
     }
